986f99dfc6cbd9fdf6a266e510708a8b5118f153
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-23 01:33:45 +0300

    test

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -26,12 +26,18 @@ function App() {
 
       <footer className={styles.footer}>
         <p className={styles.p}>
-          It’s Hossam Hassan! <br />
-            A react developer & founder of NORQUE STUDIOS™. I
-          build fast, polished interfaces — clean architecture, smooth
-          animations, and pixel-perfect Figma handoffs.
+          It’s Hossam ! <br />a front end developer & founder of NORQUE
+          STUDIOS™. I build fast, polished interfaces — clean architecture,
+          smooth animations, and pixel-perfect Figma handoffs.
         </p>
       </footer>
+
+      <div className={styles.footerBottom}>
+        <span className={styles.footerLink}>↘ LINKED IN</span>
+        <span className={styles.footerLink}>↘ GITHUB</span>
+        <span className={styles.footerLink}>↘ RESUME</span>
+        <span className={styles.footerRights}>ALL RIGHTS RESERVED</span>
+      </div>
     </div>
   );
 }
