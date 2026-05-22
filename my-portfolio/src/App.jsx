e5bb724e99dd10c7e063f7759d9b74d3dd21317a
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-22 23:52:24 +0300

    Refactor App component structure and update header styles for improved layout

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,11 +1,23 @@
-import styles from './app.module.css';
-import 
+import styles from "./app.module.css";
+import logo from "./assets/Hossam Logo 1.svg";
 
 function App() {
-  return <>
-  <header className={styles.header}></header> 
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.left}>
+          <img src={logo} alt="Hossam Logo" />
+          <span>11:26 PM CAIRO, EGYPT</span>
+        </div>
 
-  </>;
+        <div className={styles.spacer}></div>
+
+        <div className={styles.title}>PERSONAL PORTFOLIO</div>
+
+        <div className={styles.cta}>LET'S TALK NOW</div>
+      </header>
+    </>
+  );
 }
 
 export default App;
