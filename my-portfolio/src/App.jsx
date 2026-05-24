58bd917d487e7cf6fda90a6f03f6c89e774ad789
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-24 15:55:08 +0300

    Reduce SVG icon size in App component for better alignment

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -34,8 +34,8 @@ function App() {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 256 256"
-              width="20"
-              height="20"
+              width="18"
+              height="18"
               fill="#888888"
               style={{ flexShrink: 0 }}
             >
