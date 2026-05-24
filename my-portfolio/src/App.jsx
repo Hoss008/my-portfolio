f95a0f5f0ec5d8210c7cbcd4a3cde0fff0c5f067
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-05-24 15:53:17 +0300

    Enhance header layout with location icon and improve CSS for location group

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -29,13 +29,27 @@ function App() {
       <header className={styles.header}>
         <div className={styles.left}>
           <img src={logo} alt="Hossam Logo" />
-          <span className={styles.text}>{time} CAI, EGYPT</span>
+          <div className={styles.locationGroup}>
+            <span className={styles.text}>{time} CAI, EGYPT</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 256"
+              width="20"
+              height="20"
+              fill="#888888"
+              style={{ flexShrink: 0 }}
+            >
+              <g weight="regular">
+                <path d="M128,64a40,40,0,1,0,40,40A40,40,0,0,0,128,64Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,128Zm0-112a88.1,88.1,0,0,0-88,88c0,31.4,14.51,64.68,42,96.25a254.19,254.19,0,0,0,41.45,38.3,8,8,0,0,0,9.18,0A254.19,254.19,0,0,0,174,200.25c27.45-31.57,42-64.85,42-96.25A88.1,88.1,0,0,0,128,16Zm0,206c-16.53-13-72-60.75-72-118a72,72,0,0,1,144,0C200,161.23,144.53,209,128,222Z"></path>
+              </g>
+            </svg>
+          </div>
         </div>
 
         <div className={styles.title}>PERSONAL PORTFOLIO</div>
 
         <button className={styles.talk}>
-            GET IN TOUCH
+          GET IN TOUCH
           <img src={arrow} alt="Arrow" />
         </button>
       </header>
@@ -44,6 +58,7 @@ function App() {
         {" "}
         {/* TODO
         <li>skills</li>
+        loading screen ?</li>
         <li>lets talk button ? mail & phone number ?</li> */}
       </main>
 
